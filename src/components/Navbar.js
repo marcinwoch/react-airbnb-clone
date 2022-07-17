7e9de3a1cda525e4b1dfc19cf111cbaac7c302dd
Marcin Woch <marcin.woch@outlook.com>
2022-07-17 11:44:55 +0200

    Finish navbar section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,32 +11,37 @@ function Navbar() {
       </section>
       <section className="nav-images">
         <div className="col-1">
-          <div className='item'>1</div>
-          <div className='item'>1</div>
-          <div className='item'>1</div>
+          <div className='item img-1'></div>
+          <div className='item img-2'></div>
+          <div className='item img-3'></div>
         </div>
         <div className="col-2">
-          <div className='item'>2</div>
-          <div className='item'>3</div>
+          <div className='item img-4'></div>
+          <div className='item img-5'></div>
         </div>
         <div className="col-3">
-          <div className='item'>3</div>
-          <div className='item'>2</div>
+          <div className='item img-6'></div>
+          <div className='item img-7'></div>
         </div>
         <div className="col-1">
-          <div className='item'>1</div>
-          <div className='item'>1</div>
-          <div className='item'>1</div>
+          <div className='item img-8'></div>
+          <div className='item img-9'></div>
+          <div className='item img-10'></div>
         </div>
         <div className="col-2">
-          <div className='item'>2</div>
-          <div className='item'>3</div>
+          <div className='item img-11'></div>
+          <div className='item img-12'></div>
         </div>
         <div className="col-3">
-          <div className='item'>3</div>
-          <div className='item'>2</div>
+          <div className='item img-13'></div>
+          <div className='item img-14'></div>
         </div>
       </section>
+      <section className="nav-heading">
+        <h1>Online Experiences</h1>
+        <h3>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</h3>
+      </section>
+
     </nav>
   );
 }
